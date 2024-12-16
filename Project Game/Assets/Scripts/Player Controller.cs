3a6b85c0e73f6ce0d9ee0a85e25a5e204cb7d553
NichaKongkan <nicha.ko@kkumail.com>
2024-12-16 17:06:17 +0700

    add pic player, mouse movement

--- Project Game/Assets/Scripts/Player Controller.cs
+++ Project Game/Assets/Scripts/Player Controller.cs
@@ -12,27 +12,60 @@ public class PlayerController : MonoBehaviour
     private Vector2 movement;
     private Rigidbody2D rb;
 
-    private void Awake() {
+
+    private Animator myAnimator;
+    private SpriteRenderer mySpriteRender;
+
+    private void Awake()
+    {
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRender = GetComponent<SpriteRenderer>();
     }
 
-    private void OnEnable() {
+    private void OnEnable()
+    {
         playerControls.Enable();
     }
-    private void Update() {
+
+    private void Update()
+    {
         PlayerInput();
     }
 
-    private void FixedUpdate() {
+    private void FixedUpdate()
+    {
+        AdjustPlayerFacingDirection();
         Move();
     }
 
-    private void PlayerInput() {
+    private void PlayerInput()
+    {
         movement = playerControls.Movement.Move.ReadValue<Vector2>();
+
+        myAnimator.SetFloat("moveX", movement.x);
+        myAnimator.SetFloat("moveY", movement.y);
+
     }
 
-    private void Move() {
+    private void Move()
+    {
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    private void AdjustPlayerFacingDirection()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+
+        if (mousePos.x < playerScreenPoint.x)
+        {
+            mySpriteRender.flipX = true;
+        }
+        else
+        {
+            mySpriteRender.flipX = false;
+        }
     }
 }
